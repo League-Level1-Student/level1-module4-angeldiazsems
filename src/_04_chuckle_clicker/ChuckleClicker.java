ef6d3dfb3c6b77291d91c0973e601203dcc965f2
student <student@ip-172-31-62-65.us-west-2.compute.internal>
2021-05-31 13:39:55 -0700

    chuckle

--- src/_04_chuckle_clicker/ChuckleClicker.java
+++ src/_04_chuckle_clicker/ChuckleClicker.java
@@ -1,27 +1,38 @@
 package _04_chuckle_clicker;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class ChuckleClicker {
+public class ChuckleClicker implements ActionListener {
 	public static void main(String[] args) {
 		ChuckleClicker chuckle = new ChuckleClicker();
 		chuckle.makeButtons();
 	}
-
+	JButton button1 = new JButton("joke");
+	JButton button2 = new JButton("punchline");
  void makeButtons(){
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
-	JButton button1 = new JButton("joke");
-	JButton button2 = new JButton("punchline");
+	
 	frame.setVisible(true);
 	frame.setSize(100, 100);
 	panel.add(button2);
 	panel.add(button1);
 	frame.add(panel);
-	
+	button1.addActionListener(this);
+	button2.addActionListener(this);
+}
+
+@Override
+public void actionPerformed(ActionEvent arg0) {
+	// TODO Auto-generated method stub
+	JOptionPane.showMessageDialog(null, "hi");
+	if(event.getSource() == button1);
 	
 }
  
