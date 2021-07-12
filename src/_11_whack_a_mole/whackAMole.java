2f0aee3988bb48a5cd43a804e02b91ae17cc966a
student <student@ip-172-31-56-219.us-west-2.compute.internal>
2021-07-12 13:26:31 -0700

    mole

--- src/_11_whack_a_mole/whackAMole.java
+++ src/_11_whack_a_mole/whackAMole.java
@@ -1,7 +1,37 @@
 package _11_whack_a_mole;
 
+import java.awt.GridLayout;
+import java.util.Random;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class whackAMole {
-JFrame frame  = new JFrame();
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton moleButton;
+	Random ran = new Random();
+
+	void buttonMaker(Random ran) {
+		ran.nextInt(25);
+		for (int i = 0; i < 25; i++) {
+
+			JButton button = new JButton();
+			panel.add(button);
+			
+		
+		}
+	}
+
+	void setup() {
+		frame.setVisible(true);
+		frame.add(panel);
+		frame.setSize(500, 500);
+
+		panel.setLayout(new GridLayout(5, 5));
+		buttonMaker(ran);
+
+	}
+
 }
---
+++ src/_11_whack_a_mole/whackAMoleRunner.java
@@ -0,0 +1,9 @@
+package _11_whack_a_mole;
+
+public class whackAMoleRunner {
+	public static void main(String[] args) {
+
+		whackAMole whack = new whackAMole();
+		whack.setup();
+	}
+}
